85d944ba1043a429c95b18a0405f1be7c5250b19
Lukasz Komosa <lukasz.komosa@komluk.com>
2021-02-06 19:58:37 +0100

    Javascript changes

--- src/HttpTrigger/index.js
+++ src/HttpTrigger/index.js
@@ -7,7 +7,6 @@ module.exports = async function (context, req) {
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 }
